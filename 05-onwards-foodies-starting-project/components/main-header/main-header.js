85d47036520ab4597d4099ccee47789cc300574b
JavierMantecon <jmantecon@freepik.com>
2024-10-04 14:06:18 +0200

    feat: using client components efficiently

--- 05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -3,8 +3,10 @@ import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css"
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "@/components/main-header/nav-link";
 
 export default function MainHeader() {
+
   return <>
     <header className={classes.header}>
     <MainHeaderBackground />
@@ -14,10 +16,14 @@ export default function MainHeader() {
     </Link>
     <nav className={classes.nav}>
       <ul>
-        <li><Link href="/meals">Browse Meals</Link></li>
-        <li><Link href="/community">Foodies community</Link></li>
+        <li>
+          <NavLink href="/meals">Browse Meals</NavLink>
+        </li>
+        <li>
+          <NavLink href="/community">Foodies Community</NavLink>
+        </li>
       </ul>
     </nav>
-  </header>
-    </>
+    </header>
+  </>
 }
