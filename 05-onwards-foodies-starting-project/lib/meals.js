e3386b97399c1e83bee1dde774479314a9150879
JavierMantecon <jmantecon@freepik.com>
2024-10-09 18:36:38 +0200

    feat: Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- 05-onwards-foodies-starting-project/lib/meals.js
+++ 05-onwards-foodies-starting-project/lib/meals.js
@@ -4,5 +4,11 @@ const db = sql('meals.db');
 
 export async function getMeals() {
   await new Promise(resolve => setTimeout(resolve, 2000));
+  // throw new Error('Loading meals failed'); // testing error.js, error.js needs to be client component to CATCH'EM ALL
   return db.prepare('SELECT * FROM meals').all();
+}
+
+export function getMeal(slug) {
+  console.log(slug);
+  return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
 }
