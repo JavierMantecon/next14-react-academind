f0b968865480e4f61af6b0b93266bc2eb205b2ca
JavierMantecon <jmantecon@freepik.com>
2024-11-21 12:02:23 +0100

    feat(master): Handling not fund errors & showing not found pages

--- 03-routing-rendering/app/news/[slug]/page.js
+++ 03-routing-rendering/app/news/[slug]/page.js
@@ -1,9 +1,13 @@
 import {DUMMY_NEWS} from "@/dummy-news";
+import {notFound} from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
   const slug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
 
+  if(!newsItem) {
+    notFound();
+  }
   return <article className="news-article">
     <header>
       <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
