53cd276d17355cced761cc6f303a655b6bcae605
JavierMantecon <jmantecon@freepik.com>
2024-12-04 15:45:35 +0100

    fix(master): fixed header navigation

--- 03-routing-rendering/app/(content)/news/[slug]/page.js
+++ 03-routing-rendering/app/(content)/news/[slug]/page.js
@@ -9,14 +9,16 @@ export default function NewsDetailPage({params}) {
     if (!newsItem) {
         notFound();
     }
-    return <article className="news-article">
-        <header>
-            <Link href={`/app/(content)/news/${newsItem.slug}/image`}>
-                <img src={`/images/news/${newsItem.image}`} alt={newsItem.title}/>
-            </Link>
-            <h1>{newsItem.title}</h1>
-            <time dateTime={newsItem.date}>{newsItem.date}</time>
-        </header>
-        <p>{newsItem.content}</p>
-    </article>
+    return (
+        <article className="news-article">
+            <header>
+                <Link href={`/news/${newsItem.slug}/image`}>
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                </Link>
+                <h1>{newsItem.title}</h1>
+                <time dateTime={newsItem.date}>{newsItem.date}</time>
+            </header>
+            <p>{newsItem.content}</p>
+        </article>
+    );
 }
